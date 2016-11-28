2d2e38a1f340ee65807b357ccf66abdfa4bb3b6c
VICTORIA JUAN <squintyblu@gmail.com>
2016-11-27 16:08:33 -0800

    Undo start page for practice

--- main.lua
+++
@@ -1,9 +0,0 @@
---main.lua
-
-display.setStatusBar(display.HiddenStatusBar)
-
-local composer = require("composer")
-
---replace this text with options for transition text
-
-composer.gotoScene("start")
