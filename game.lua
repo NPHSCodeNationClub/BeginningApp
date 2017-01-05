2b4fa47094d1c9b387adc99278b4a7d7afb3eaea
Victoria Juan <squintyblu@gmail.com>
2017-01-04 17:04:29 -0800

    Add collision

--- game.lua
+++ game.lua
@@ -19,7 +19,20 @@ function scene:create(event)
     player.x = display.contentWidth * 0.5
     player.y = display.contentHeight * 0.5
     physics.addBody(player, "static", {density=.08,  bounce=0.1, friction=0.2})
+    transition.to(player, {time=100, x = display.contentWidth * 0.5, y = display.contentHeight * 0.5, onComplete = playerReady()})
     screenGroup:insert(player)
+    
+    floor = display.newImage("rectangle.png")
+    floor.x = display.contentWidth * 0.5
+    floor.y = display.contentHeight
+    physics.addBody(floor, "static", {density=.08,  bounce=0.1, friction=0.2})
+    screenGroup:insert(floor)
+    
+    ceiling = display.newImage("rectangle.png")
+    ceiling.x = display.contentWidth * 0.5
+    ceiling.y = 0
+    physics.addBody(ceiling, "static", {density=.08,  bounce=0.1, friction=0.2})
+    screenGroup:insert(ceiling)
     
 end
 
@@ -45,18 +58,37 @@ function touchScreen(event)
     end
 end
 
+function gameOver()
+    composer.gotoScene("start", "fade", 500)
+end
+
+function playerHit()
+    player.isVisible = false
+    timer.performWithDelay(100, gameOver, 1)
+end
+
+function onCollision(event)
+    if event.phase == "began" then
+        player.collided = true
+        player.bodyType = "static"
+        playerHit()
+    end
+end
+
 function scene:show(event)
 	if (event.phase == "will") then
 		composer.removeScene("start")
 
 	elseif (event.phase == "did") then
 		Runtime:addEventListener("touch", touchScreen)
+        Runtime:addEventListener("collision", onCollision)
 	end
 end
 
 function scene:hide(event)
 	if (event.phase == "will") then
         Runtime:removeEventListener("touch", touchScreen)
+        Runtime:removeEventListener("collision", onCollision)
 
 	elseif (event.phase == "did") then
 
